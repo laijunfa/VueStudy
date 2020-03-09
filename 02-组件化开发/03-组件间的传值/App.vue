6b0ff710efe504c099572d1a2679f5a2b83c01d1
laijunfa <1570292991@qq.com>
2020-03-09 11:06:15 +0800

    模板分离

---
+++ 02-组件化开发/03-组件间的传值/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div>{{msg}}</div>
+    <button @click="getSon">获取子组件的值</button>
+    <son ref="son"></son>
+  </div>
+</template>
+
+<script>
+//父组件传值
+//1.
+import son from "./components/son.vue";
+export default {
+  data() {
+    return {
+      msg: "我是父组件"
+    };
+  },
+  components: {
+    son
+  },
+  methods: {
+    getSon() {
+        console.log('子组件的值',this.$refs.son.msg);
+        this.$refs.son.created();
+    }
+  },
+  created(){
+      console.log('父组件created');
+  },
+  mounted() {
+     console.log('父组件mounted');
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 02-组件化开发/03-组件间的传值/components/son.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="sonBox">
+    <div>{{msg}}</div>
+    <button @click="getFather">获取父组件的值</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "我是子组件"
+    };
+  },
+  methods: {
+    getFather() {
+      console.log("获取父组件的值", this.$parent.msg);
+    }
+  },
+  created() {
+     console.log("子组件created");
+  },
+  mounted() {
+    console.log("子组件mounted");
+  }
+};
+</script>
+
+<style>
+.sonBox {
+  width: 400px;
+  height: 400px;
+  border: 1px solid red;
+}
+</style>
